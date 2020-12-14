593b13a99c28f3fe7b53dc286c096b85060b2092
vito <gralle89@gmail.com>
2020-12-14 12:30:06 +0100

    Netlify Identity and CNAME

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,13 @@
 </template>
 
 <script>
-export default {}
+export default {
+  head () {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+    }
+  }
+}
 </script>
 
 <style>
