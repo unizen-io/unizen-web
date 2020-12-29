c062ecf70bdbd100e14ffd915d92c70c06c7fb63
vito <gralle89@gmail.com>
2020-12-29 21:53:02 +0100

    Added team page

--- pages/blog.vue
+++ pages/blog.vue
@@ -22,5 +22,6 @@ export default {
 <style lang="scss" scoped>
 .main {
     background-image: linear-gradient(to bottom, #f5f5f5, #f4f4f4, #f2f3f2, #f1f1f1, #f0f0f0);
+    min-height: 1000px;
 }
 </style>
