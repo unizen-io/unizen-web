a6e36f499ba0f1029b464c0bdd9e08243e27dcc1
vito <gralle89@gmail.com>
2020-12-30 11:54:22 +0100

    added contact page

---
+++ pages/contact.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <b-row>
+      <b-container class="py-5 my-5">
+        <h1 class="blog py-5 mt-5">
+          Contact.
+        </h1>
+        <br>
+        <b-col />
+        <b-col>
+          <b-card>
+            We would love to talk to you about any business inquiry or partner proposal. If this relates to support for any of our exchange modules, please reach out to support@zencex.com.
+          </b-card>
+          <br>
+          <b-form-group data-netlify-recaptcha="true" name="contact" method="POST" data-netlify="true">
+            <b-form-input
+              id="input-1"
+              v-model="name"
+              novalidate="true"
+              placeholder="What's your name?"
+              size="lg"
+              :state="nameState"
+              trim
+            />
+            <br>
+            <b-form-input
+              id="input-2"
+              v-model="email"
+              placeholder="What's your e-mail?"
+              type="email"
+              name="email"
+              size="lg"
+              :state="emailState"
+              trim
+            />
+            <br>
+            <b-form-textarea
+              id="textarea-no-resize"
+              v-model="text"
+              placeholder="Enter a message..."
+              rows="12"
+              size="lg"
+              :state="textState"
+              name="message"
+            />
+            <br>
+            <div data-netlify-recaptcha="true" />
+            <b-button variant="outline-primary" size="lg" class="contact" type="submit">
+              Send
+            </b-button>
+          </b-form-group>
+        </b-col>
+        <b-col />
+      </b-container>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: '',
+      email: '',
+      text: '',
+      reg: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/
+
+    }
+  },
+  computed: {
+    nameState () {
+      if (this.name.length >= 4) {
+        return true
+      } else {
+        return null
+      }
+    },
+    textState () {
+      if (this.text.length >= 10) {
+        return true
+      } else {
+        return null
+      }
+    },
+
+    invalidFeedback () {
+      if (this.name.length > 0) {
+        return 'Enter at least 4 characters.'
+      }
+      return 'Please enter something.'
+    },
+    emailState () {
+      if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(this.email)) {
+        return true
+      } else {
+        return null
+      }
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+.contact {
+    width: 100% !important;
+}
+</style>
