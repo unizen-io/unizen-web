971522c89be825c5a9cdede2c8506bab90c1e2c8
anton-karlovskiy <antonkarlovskiy@outlook.com>
2021-03-16 09:52:45 +0300

    [infra] set up CONTACT_US_FORM_SUBMISSION_ENDPOINT

--- config/index.js
+++ config/index.js
@@ -1,6 +1,5 @@
 
-// TODO: should use an environment variable
-const CONTACT_US_FORM_SUBMISSION_ENDPOINT = 'https://formcarry.com/s/TV0DUuMk_s'
+const CONTACT_US_FORM_SUBMISSION_ENDPOINT = process.env.CONTACT_US_FORM_SUBMISSION_ENDPOINT
 
 const LC_API_KEY = process.env.NUXT_ENV_LC_API
 
