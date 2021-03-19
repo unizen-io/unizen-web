e7915477db1457f0a300194be6b2b8acd1eb4851
anton-karlovskiy <antonkarlovskiy@outlook.com>
2021-03-19 20:49:18 +0300

    [chore] log at ipx

--- api/ipx.js
+++ api/ipx.js
@@ -9,7 +9,11 @@ const ipx = createIPX({})
 const middleware = createIPXMiddleware(ipx)
 
 export default (req, res) => {
-  req.url = `/assets/img/${req.url}`
+  req.url = `/assets/img${req.url}`
+
+  // ray test touch <
+  console.log('ray : ***** req.url => ', req.url)
+  // ray test touch >
 
   return middleware(req, res)
 }
