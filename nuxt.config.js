2d79f36c701827aaa215098835d5b20151f94cf8
anton-karlovskiy <antonkarlovskiy@outlook.com>
2021-03-21 16:23:19 +0300

    [fix] avoid pwa overrides in favor of default setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,20 +71,6 @@ export default {
   //   dev: false
   // },
 
-  pwa: {
-    icon: {
-      sizes: [
-        64,
-        192,
-        512
-      ]
-    },
-    meta: {
-      theme_color: '#ffffff'
-    },
-    manifest: false
-  },
-
   sitemap: {
     hostname: 'https://unizen.io',
     gzip: true,
