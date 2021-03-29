94125efe6d73abafbb89c75c49ce8357273ae5d0
anton-karlovskiy <antonkarlovskiy@outlook.com>
2021-03-29 23:24:48 +0300

    [feat] implement aos on the thanks page

--- pages/thanks.vue
+++ pages/thanks.vue
@@ -3,7 +3,11 @@
     style="background-image: linear-gradient(to bottom, #f5f5f5, #f4f4f4, #f2f3f2, #f1f1f1, #f0f0f0);"
     class="min-h-screen"
   >
-    <div class="container mx-auto px-8 pt-48">
+    <div
+      class="container mx-auto px-8 pt-48"
+      data-aos="fade-up"
+      data-aos-duration="2000"
+    >
       <h1
         style="font-family: Montserrat Medium !important;"
         class="contact font-bold text-center mt-24 mb-2 text-tertiary"
