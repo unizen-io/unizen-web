e0cb02eae7f3c70559a3dbdcfea612fe6010a86c
anton-karlovskiy <antonkarlovskiy@outlook.com>
2021-03-30 16:21:27 +0300

    [fix] correct markups on the privacy-policy page

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -18,22 +18,20 @@
         data-aos="fade-up"
         data-aos-duration="2000"
       >
-        <div class="container mx-auto px-8">
-          <p
-            style="font-family: Montserrat Medium;"
-            class="text-base"
-          >
-            Cookies are small text files that are placed on your computer by websites that you visit. They are widely used in order to make websites work, or work more efficiently, as well as to provide information to the owners of the site. <br><br>
-            If you chose to opt in, the ZEN Team will use the following Google Analytics cookies to collect information on how you are using our site: <br><br>
-            <ul>
-              <li>_ga</li>
-              <li>_gid</li>
-            </ul>
-            <br>
-            The information collected using these cookies will allow me to see which articles are sesonating with my readers and enable me to improve the site and write better, more useful content.<br><br>
-            You can read an overview of Google's privacy overview and data safeguarding article <a href="https://policies.google.com/privacy?hl=en-US">here</a>.<br><br>
-            You can also download a <a href="https://chrome.google.com/webstore/detail/google-analytics-opt-out/fllaojicojecljbmefodhfapmkghcbnh">browser add-on</a> from Google to opt out of Google Analytics entirely.
-          </p>
+        <div
+          style="font-family: Montserrat Medium;"
+          class="container mx-auto px-8 text-base"
+        >
+          Cookies are small text files that are placed on your computer by websites that you visit. They are widely used in order to make websites work, or work more efficiently, as well as to provide information to the owners of the site. <br><br>
+          If you chose to opt in, the ZEN Team will use the following Google Analytics cookies to collect information on how you are using our site: <br><br>
+          <ul>
+            <li>_ga</li>
+            <li>_gid</li>
+          </ul>
+          <br>
+          The information collected using these cookies will allow me to see which articles are sesonating with my readers and enable me to improve the site and write better, more useful content.<br><br>
+          You can read an overview of Google's privacy overview and data safeguarding article <a href="https://policies.google.com/privacy?hl=en-US">here</a>.<br><br>
+          You can also download a <a href="https://chrome.google.com/webstore/detail/google-analytics-opt-out/fllaojicojecljbmefodhfapmkghcbnh">browser add-on</a> from Google to opt out of Google Analytics entirely.
         </div>
       </b-card>
     </div>
