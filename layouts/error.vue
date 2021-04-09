aba65a637c0173b289cb0a85e00243be32e63bcc
anton-unizen <anton.karlovskiy.g@gmail.com>
2021-04-09 10:36:43 +0300

    [fix] clean up

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,21 +1,17 @@
 <template>
-  <div class="layout-default">
-      <div class="pt-4 mt-12">
-        <center>
-            <div class="mt-12 pt-12">
-                <h1 v-if="error.statusCode === 404">This page could not be found</h1>
-                <h1 v-else>There was an unknown error</h1>
-            </div>
-        </center>
-      </div>
-      <div tyle="padding-bottom: 200px;" />
-      <br><br><br>
-  </div>
+  <h1 class="mt-40 text-center">
+    {{ error.statusCode === 404 ? 'This page could not be found.' : 'There was an unknown error.' }}
+  </h1>
 </template>
 
 <script>
 
 export default {
-  props: ['error']
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  }
 }
 </script>
