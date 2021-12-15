4bf24e3fcb140f3debf840887cf82e79895a7d86
vito <gralle89@gmail.com>
2021-12-15 13:40:07 +0100

    fix: removed aos from graphs

--- pages/competition.vue
+++ pages/competition.vue
@@ -44,8 +44,6 @@
       <div
         style="background: #efefef;"
         class="container mx-auto px-8"
-        data-aos="fade-in"
-        data-aos-duration="2000"
       >
         <div class="mt-12 flex flex-wrap">
           <div class="px-4 w-full md:w-1/2">
@@ -89,20 +87,14 @@
       <ZtiMetrics
         :first-competitor="FirstCompetitor"
         :second-competitor="SecondCompetitor"
-        data-aos="fade-up"
-        data-aos-duration="2000"
       />
       <ZsiMetrics
         :first-competitor="FirstCompetitor"
         :second-competitor="SecondCompetitor"
-        data-aos="fade-up"
-        data-aos-duration="2000"
       />
       <ZssMetrics
         :first-competitor="FirstCompetitor"
         :second-competitor="SecondCompetitor"
-        data-aos="fade-up"
-        data-aos-duration="2000"
       />
     </div>
   </div>
