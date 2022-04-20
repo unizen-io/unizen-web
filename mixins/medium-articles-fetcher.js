fef6642fdddbd4a0dec6cb84354dbacc5def5479
jyustecastell <jyustecastell@gmail.com>
2022-04-20 08:35:39 +0200

    fix: test 6

--- mixins/medium-articles-fetcher.js
+++ mixins/medium-articles-fetcher.js
@@ -3,17 +3,22 @@ import STATUSES from '@/utils/constants/statuses'
 import transformMediumArticles from '@/utils/helpers/transform-medium-articles'
 
 const mediumArticlesFetcherMixin = {
-  async fetch () {
-    try {
-      this.status = STATUSES.PENDING
-      const data = await this.$axios.$get(
-        '/medium'
-      )
-      this.articles = transformMediumArticles(data)
-      this.status = STATUSES.RESOLVED
-    } catch (error) {
-      this.status = STATUSES.REJECTED
-      this.error = error
+  mounted () {
+    this.fetchArticles()
+  },
+  methods: {
+    async fetchArticles () {
+      try {
+        this.status = STATUSES.PENDING
+        const data = await this.$axios.$get(
+          '/medium'
+        )
+        this.articles = transformMediumArticles(data)
+        this.status = STATUSES.RESOLVED
+      } catch (error) {
+        this.status = STATUSES.REJECTED
+        this.error = error
+      }
     }
   }
 }
