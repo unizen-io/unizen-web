81544244a52b63acaefe8a06a10e6acd5c4fc13e
jyustecastell <jyustecastell@gmail.com>
2022-05-20 10:40:56 +0200

    fix: added twitter snippet

--- plugins/twitter.js
+++ plugins/twitter.js
@@ -3,6 +3,7 @@ export default () => {
   // Add script tag to head
   const script = document.createElement('script')
   script.id = 'twitter'
+  script.async = true
   script.src = '/js/twitterSnippet.js'
   document.head.appendChild(script)
 }
